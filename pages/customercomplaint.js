58d17b3589a81f72d1a02c6157373a7ffb9db66b
Arvind Yadav <arvindyadav@Arvinds-MacBook-Pro.local>
2022-07-06 15:12:06 +0530

    small change

--- pages/customercomplaint.js
+++ pages/customercomplaint.js
@@ -7,7 +7,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import {getData,useCount} from '../auth/reducer'
+import {getData,useCount} from '../../auth/reducer'
 import { useEffect} from 'react';
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
