f6d5628a7d627a3795f2b639d6031d77df8240bd
Harsh <harshuniverse000@gmail.com>
2023-04-04 12:15:51 +0530

    Completed Room edit route

--- pages/_app.js
+++ pages/_app.js
@@ -22,6 +22,7 @@ import { TrackingLogsStateProvider } from "../auth/trackingLogsReducer";
 import { SchedulesStateProvider } from "../auth/scheduleReducer";
 import { SmifiInfoStateProvider } from "../auth/smifiReducer";
 import { CouponsProvider } from "../auth/couponsReducer";
+import { RoomProvider } from "../auth/RoomsDataReducer";
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
 
@@ -44,25 +45,27 @@ export default function MyApp(props) {
               <TotalUserProvider>
                 <DeviceProvider>
                   <RoomPinsProvider>
-                    <SubStateProvider>
-                      <UserInformationStateProvider>
-                        <PremiseUserStateProvider>
-                          <PremiseRoomsStateProvider>
-                            <TrackingLogsStateProvider>
-                              <SchedulesStateProvider>
-                                <SmifiInfoStateProvider>
-                                  <CouponsProvider>
-                                    {/* <Layout userRole={userRole}> */}
-                                    {/* {getLayout(<Component {...pageProps} />)} */}
-                                    <Component {...pageProps} />
-                                  </CouponsProvider>
-                                </SmifiInfoStateProvider>
-                              </SchedulesStateProvider>
-                            </TrackingLogsStateProvider>
-                          </PremiseRoomsStateProvider>
-                        </PremiseUserStateProvider>
-                      </UserInformationStateProvider>
-                    </SubStateProvider>
+                    <RoomProvider>
+                      <SubStateProvider>
+                        <UserInformationStateProvider>
+                          <PremiseUserStateProvider>
+                            <PremiseRoomsStateProvider>
+                              <TrackingLogsStateProvider>
+                                <SchedulesStateProvider>
+                                  <SmifiInfoStateProvider>
+                                    <CouponsProvider>
+                                      {/* <Layout userRole={userRole}> */}
+                                      {/* {getLayout(<Component {...pageProps} />)} */}
+                                      <Component {...pageProps} />
+                                    </CouponsProvider>
+                                  </SmifiInfoStateProvider>
+                                </SchedulesStateProvider>
+                              </TrackingLogsStateProvider>
+                            </PremiseRoomsStateProvider>
+                          </PremiseUserStateProvider>
+                        </UserInformationStateProvider>
+                      </SubStateProvider>
+                    </RoomProvider>
                   </RoomPinsProvider>
                 </DeviceProvider>
               </TotalUserProvider>
